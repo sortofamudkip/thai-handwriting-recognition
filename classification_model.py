362b3334d88ee168a9438e3ac1de8a10f7c5714b
wishyut.pitawanik <wishyutp0328@gmail.com>
2024-02-19 11:51:06 +0100

    classification CNN model

---
+++ classification_model.py
@@ -0,0 +1,77 @@
+import numpy as np
+from tensorflow import keras
+
+DATA_AUGMENTATION_LAYERS = [
+    keras.layers.RandomZoom(-0.2),
+    keras.layers.RandomTranslation(0.1, 0.1),
+]
+
+def data_augmentation(images):
+    for layer in DATA_AUGMENTATION_LAYERS:
+        images = layer(images)
+    return images
+
+
+# define CNN classification model
+def create_classification_model(input_shape, num_classes, is_use_augmentation):
+    inputs = keras.Input(shape=input_shape)
+    # * rescalse the input
+    x = keras.layers.Rescaling(1./255)(inputs)
+    # * apply data augmentation
+    if is_use_augmentation:
+        x = data_augmentation(x)
+    # * convolutional layers
+    x = keras.layers.Conv2D(32, 3, activation='relu')(inputs)
+    x = keras.layers.MaxPooling2D()(x)
+    x = keras.layers.Conv2D(64, 3, activation='relu')(x)
+    x = keras.layers.MaxPooling2D()(x)
+    x = keras.layers.Conv2D(128, 3, activation='relu')(x)
+    x = keras.layers.MaxPooling2D()(x)
+    # * fully connected layers
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dropout(0.10)(x)
+    x = keras.layers.Dense(128, activation='relu')(x)
+    # * output layer (softmax activation function for multiclass classification)
+    outputs = keras.layers.Dense(num_classes, activation='softmax')(x)
+    return keras.Model(inputs, outputs)
+
+def train_model(model, train_dataset, validation_dataset, epochs=15):
+    # create a token consisting of 4 random characters
+    token = ''.join(np.random.choice(list('abcdefghijklmnopqrstuvwxyz'), 4)) 
+
+    # compile the model
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()]
+    )
+
+    # create a callback to stop training if the validation accuracy does not improve for 3 epochs
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor='val_sparse_categorical_accuracy',
+        patience=3,
+        restore_best_weights=True
+    )
+    # create a callback to save the model on the best validation accuracy
+    model_checkpoint = keras.callbacks.ModelCheckpoint(
+        filepath=f'model-{token}.h5',
+        monitor='val_sparse_categorical_accuracy',
+        save_best_only=True,
+        save_weights_only=True,
+    )
+
+    print(f"🟢Token: {token}")
+    # train the model
+    history = model.fit(
+        train_dataset,
+        validation_data=validation_dataset,
+        epochs=epochs,
+        callbacks=[early_stopping, model_checkpoint]
+    )
+    return history
+
+
+if __name__ == '__main__':
+    pass
+    # create the model
+    # model = create_model((64, 64, 1), len(class_names)) # class_names is 42
