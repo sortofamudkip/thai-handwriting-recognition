8866e2dc5bda4fddabea75a3bdb102c773e4dc66
wishyut.pitawanik <29839553+sortofamudkip@users.noreply.github.com>
2024-02-29 23:28:42 +0100

    autoencoder model and pipeline update

--- autoencoder/autoencoder_model.py
+++ autoencoder/autoencoder_model.py
@@ -2,33 +2,41 @@ from tensorflow import keras
 from pathlib import Path
 
 # define an autoencoder model that takes 64x64x1 images and returns 64x64x1 images
-def build_autoencoder_model(autoencoder_latent_dim:int, input_shape=(64, 64, 1)):
+def build_autoencoder_model(input_shape=(64, 64, 1), learning_rate=1e-3) -> keras.Model:
     input_img = keras.layers.Input(shape=input_shape)
+    
     # Encoder
-    x = keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same")(input_img)
+    x = keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same")(input_img)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.MaxPooling2D((2, 2), padding="same")(x)
-    x = keras.layers.Conv2D(8, (3, 3), activation="relu", padding="same")(x)
+    
+    x = keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same")(x)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.MaxPooling2D((2, 2), padding="same")(x)
-    x = keras.layers.Conv2D(8, (3, 3), activation="relu", padding="same")(x)
-    x = keras.layers.MaxPooling2D((2, 2), padding="same")(x)
-    x = keras.layers.Flatten()(x)
-    encoded = keras.layers.Dense(autoencoder_latent_dim, activation="relu")(x)
-
+    
+    x = keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same")(x)
+    x = keras.layers.BatchNormalization()(x)
+    encoded = keras.layers.MaxPooling2D((2, 2), padding="same")(x)
+    
     # Decoder
-    x = keras.layers.Dense(8*8*16, activation="relu")(encoded)
-    x = keras.layers.Reshape((8, 8, 16))(x)
-    x = keras.layers.Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same")(x)
-    x = keras.layers.Conv2DTranspose(16, (3, 3), strides=2, activation="relu", padding="same")(x)
-    x = keras.layers.Conv2DTranspose(8, (3, 3), strides=2, activation="relu", padding="same")(x)
-    x = keras.layers.Conv2D(1, (3, 3), activation="sigmoid", padding="same")(x)
-
-
-    # get the autoencoder model
-    autoencoder = keras.Model(input_img, x)
-    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    x = keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same")(encoded)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+    
+    x = keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same")(x)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+    
+    x = keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same")(x)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+    
+    decoded = keras.layers.Conv2D(1, (3, 3), activation="sigmoid", padding="same")(x)
+    
+    autoencoder = keras.Model(input_img, decoded)
+    autoencoder.compile(
+        optimizer=keras.optimizers.Adam(learning_rate),
+        loss=keras.losses.MeanSquaredError(),
+    )
     autoencoder.summary()
 
-    # also get the encoder model
     encoder = keras.Model(input_img, encoded)
 
     return encoder, autoencoder
@@ -42,15 +50,15 @@ def train_model(
         batch_size=64,
     ) -> keras.callbacks.History:
 
-    # # create a callback to stop training if the validation accuracy does not decrease for 3 epochs
-    # early_stopping = keras.callbacks.EarlyStopping(
-    #     monitor='val_loss',
-    #     patience=3,
-    #     restore_best_weights=True
-    # )
+    # create a callback to stop training if the validation accuracy does not decrease
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=5,
+        restore_best_weights=True
+    )
     # create a callback to save the model on the best validation accuracy
     model_checkpoint = keras.callbacks.ModelCheckpoint(
-        filepath= model_path/f'model.h5',
+        filepath= Path(model_path)/f'model.h5',
         monitor='val_loss',
         save_best_only=True,
         save_weights_only=True,
@@ -64,9 +72,11 @@ def train_model(
         validation_data=(validation_images, validation_images),
         epochs=epochs,
         batch_size=batch_size,
-        callbacks=[model_checkpoint],
+        callbacks=[model_checkpoint,early_stopping],
     )
 
     return history
+
+
 if __name__ == "__main__":
     encoder, autoencoder = build_autoencoder_model(128)
--- autoencoder_pipeline.py
+++ autoencoder_pipeline.py
@@ -22,9 +22,11 @@ def run_autoencoder_pipeline(
         pipeline_name: str,
         dataset_path: Path,
         num_epochs:int,
+        batch_size:int,
+        learning_rate:float,
     ):
 
-    output_dir = create_output_dir(pipeline_name, skip_if_exists=False)
+    output_dir = create_output_dir(pipeline_name, "autoencoding_jobs", skip_if_exists=False)
     output_file_name = str((output_dir / f"log.log").resolve())
    
     logging.basicConfig(
@@ -37,25 +39,66 @@ def run_autoencoder_pipeline(
 
     # load the data
     train_images, validation_images, test_images = load_images_for_autoencoder(dataset_path/'train', dataset_path/'test')
-    # normalise the data in [0, 1]
-    train_images = train_images / 255.0
-    validation_images = validation_images / 255.0
-    test_images = test_images / 255.0
+    # normalise the data in [0, 1] and reverse the colors
+    train_images = 1 - (train_images / 255.0)
+    validation_images = 1 - (validation_images / 255.0)
+    test_images = 1 - (test_images / 255.0)
 
     # create and compile the model
-    encoder, autoencoder = autoencoder_model.build_autoencoder_model(128)
+    encoder, autoencoder = autoencoder_model.build_autoencoder_model(learning_rate=learning_rate)
     # train the model
-    history = autoencoder_model.train_model(autoencoder, train_images, validation_images, model_path=output_dir, epochs=num_epochs)
+    history = autoencoder_model.train_model(
+        autoencoder,
+        train_images,
+        validation_images,
+        model_path=output_dir,
+        epochs=num_epochs,
+        batch_size=batch_size
+    )
+    # save the history
+    with open(output_dir / 'history.json', 'w') as f:
+        json.dump(history.history, f)
+    # plot the history and save to file
+    history_df = pd.DataFrame(history.history)
+    history_df.plot()
+    plt.savefig(output_dir / 'history.png')
+    plt.close()
+
+    # plot the 5 first test images and their reconstructions
+    n = 5
+    reconstructed_images = autoencoder.predict(test_images[:n])
+    fig, axes = plt.subplots(2, n, figsize=(20, 5))
+    for i in range(5):
+        axes[0, i].imshow((test_images[i]), cmap='gray')
+        axes[1, i].imshow((reconstructed_images[i]), cmap='gray')
+    plt.savefig(output_dir / 'reconstructed_images.png')
+    plt.close()
+    # save the model
+    autoencoder.save(output_dir / 'model.h5')
+    # save the encoder
+    encoder.save(output_dir / 'encoder.h5')
+
 
 
 if __name__ ==  '__main__':
-    # create a token consisting of 4 random characters
-    token = ''.join(np.random.choice(list('abcdefghijklmnopqrstuvwxyz'), 4))
-    print(f"🟢Token: {token}")
-    PIPELINE_NAME = f"autoencoder-{token}"
+    # create argparse to get the model name and other parameters
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', type=int, default=15, help='The number of epochs to train the model')
+    parser.add_argument('--batch_size', type=int, default=32, help='The batch size to use')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='The learning rate to use')
+    # parser.add_argument('--is_use_augmentation', type=bool, default=True, help='Whether to use data augmentation', action=argparse.BooleanOptionalAction)
+    # parser.add_argument('--dataset', type=str, default='processed_dataset', help='The dataset to use', choices=['processed_dataset','processed_dataset_binary'])
+    # * add an optional argument for token
+    parser.add_argument('--token', type=str, default=None, help='The token to use for the pipeline name')
+    args = parser.parse_args()
+
+    PIPELINE_NAME = f"autoencoder-{args.token if args.token else 'default'}"
 
     run_autoencoder_pipeline(
         PIPELINE_NAME,
-        Path(__file__).parent / 'processed_dataset',
-        num_epochs=15
+        Path(__file__).parent / 'processed_dataset_binary',
+        num_epochs=args.num_epochs,
+        batch_size=args.batch_size,
+        learning_rate=args.learning_rate
     )
